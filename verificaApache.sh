96f9eb2c17e24257d7676d37fdb7b5e7d8767306
Felipe <felipe.citnnet@gmail.com>
2019-11-28 16:44:26 -0300

    Update verificaApache.sh

--- verificaApache.sh
+++ verificaApache.sh
@@ -4,13 +4,13 @@ touch /tmp/log_apache.txt
 
 while true; do
     status=$(curl --write-out %{http_code} --connect-timeout 5  --silent --output /dev/null http://10.0.1.10/index.html)
-	  if [ $status -eq 200 ]; then  
-        time=`uptime | awk '{print $1}'`
-        echo "on($status): $time" >> /tmp/log_pache.txt            
-    else
-        tempo=`uptime | awk '{print $1}'`            
-        echo "off($status): $time" >> /tmp/log_pache.txt
-    fi
-    
+        if [ $status -eq 200 ]; then  
+            time=`uptime | awk '{print $1}'`
+            echo "on($status): $time" >> /tmp/log_pache.txt            
+        else
+            tempo=`uptime | awk '{print $1}'`            
+            echo "off($status): $time" >> /tmp/log_pache.txt
+        fi
+	  
 sleep 1
 done
